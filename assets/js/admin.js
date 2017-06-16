bd72163a2949bade08d4bb9c39573517b2a21800
EmanueleCoppola <emanuele.coppola99@gmail.com>
2017-06-16 16:42:11 +0200

    Added settings save function

---
+++ assets/js/admin.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Manu on 16/06/2017.
+ */
+(function($){
+    $('#advancedbtn').on('click', function(e) {
+        e.preventDefault();
+
+        $('#advancedopts').fadeToggle();
+    })
+
+})(jQuery);
